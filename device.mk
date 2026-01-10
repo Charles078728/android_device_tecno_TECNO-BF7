6533e094937f0d6be35ea46987e140c168ff005a
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-01-10 15:40:14 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,13 +19,11 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6761
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6761 \
     libgptutils \
     libz \
     libcutils
+
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
