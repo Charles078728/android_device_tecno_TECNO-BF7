19857dad21f31714e85cfd9e2bb22afeba9167be
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-01-20 21:39:23 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,13 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_BF7.mk
+    $(LOCAL_DIR)/pb_BF7.mk
 
 COMMON_LUNCH_CHOICES := \
-    fox_BF7-eng
+    pb_BF7-user \
+    pb_BF7-userdebug \
+    pb_BF7-eng
