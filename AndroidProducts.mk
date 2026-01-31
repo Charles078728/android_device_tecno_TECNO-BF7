e3fbb18922bc7ce8b5182579a53e5a7a8ed13a51
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-01-31 15:31:20 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_BF7.mk
+    $(LOCAL_DIR)/shrp_BF7.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_BF7-user \
-    twrp_BF7-userdebug \
-    twrp_BF7-eng
+     shrp_BF7-eng
