d51e88f88e95b6008d1463f555b034eafeb3e57f
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-07-29 19:31:11 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,16 +6,12 @@
 #
 
 LOCAL_PATH := device/tecno/BF7
-
-# A/B OTA Config
+# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Health HAL
 PRODUCT_PACKAGES += \
@@ -23,28 +19,33 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
     libhealthd.$(PRODUCT_PLATFORM)
 
-# MediaTek Boot Control HAL (v1.1 for MT6761)
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Boot Control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl \
     android.hardware.boot@1.1-mtkimpl.recovery \
-    bootctrl.mt6761 \
-    bootctrl.mt6761.recovery \
+    android.hardware.boot@1.1-mtkimpl \
     bootctl
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl.mt6761
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# OTA and Update Tools
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
-    update_engine_sideload \
-    update_engine_client
+    update_engine_sideload
 
-# OEM Security Keys
+# OEM otacerts
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(LOCAL_PATH)/security/tecno
