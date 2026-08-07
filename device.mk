48c952d7f3c5caa143c5eadd523aa86c07489315
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-08-07 17:15:54 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,16 +19,13 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
+    bootctrl.mt6761 \
+    bootctrl.mt6761.recovery 
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6761
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6761 \
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
