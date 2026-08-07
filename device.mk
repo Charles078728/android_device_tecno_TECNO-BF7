02d6a4a4e3c6c4c6dbd51f26a7ff1c986b3b56e1
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-08-07 15:53:51 +0800

    Refactor device.mk for TECNO-BF7 configuration
    Updated LOCAL_PATH and modified boot control HAL packages. Removed unused dynamic partitions and OEM security keys.

--- device.mk
+++ device.mk
@@ -5,41 +5,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/BF7
+LOCAL_PATH := device/tecno/TECNO-BF7
 
-# A/B OTA Config
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Health HAL
+# Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM)
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
 
-# MediaTek Boot Control HAL (v1.1 for MT6761)
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl \
-    android.hardware.boot@1.1-mtkimpl.recovery \
+    bootctrl.mt6761
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6761 \
-    bootctrl.mt6761.recovery \
-    bootctl
+    libgptutils \
+    libz \
+    libcutils
 
-# OTA and Update Tools
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
-    update_engine_sideload \
-    update_engine_client
-
-# OEM Security Keys
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(LOCAL_PATH)/security/tecno
+    update_engine_sideload
