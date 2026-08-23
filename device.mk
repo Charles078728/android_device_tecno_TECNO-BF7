f93d0fc285ad6754f444b53136af21255611a966
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-08-24 04:34:31 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -34,3 +34,12 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Crypto / Keymaster
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
